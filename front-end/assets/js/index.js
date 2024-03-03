fbe5fa9fc7a42dcbe2954c4dec67a058e60a7ca6
Ewerton Motta <contato@ewerton.dev>
2024-03-03 10:34:08 -0300

    Adicionando a visualização da data de publicação

--- front-end/assets/js/index.js
+++ front-end/assets/js/index.js
@@ -15,11 +15,16 @@ function mountPostList(posts) {
     posts.forEach((post) => {
         const postElement = document.createElement("div");
         postElement.innerHTML = `
-            <a href="${post.url}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4 p-2">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="${post.image}" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${post.title}</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${post.description}</p>
+            <a href="${post.url}" class="flex justify-between bg-white border border-gray-200 rounded-lg shadow md:flex-row md:w-full hover:bg-gray-100 my-4 p-2">
+                <div class="flex  items-center">
+                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="${post.image}" alt="">
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 flex-wrap">${post.title}</h5>
+                        <p class="mb-3 font-normal text-gray-700">${post.description}</p>
+                    </div>
+                </div>
+                <div class="mb-3 font-normal text-[0.75rem] text-gray-700w-48 flex justify-end items-end">
+                    <span>${post.published_at}</span>
                 </div>
             </a>
             `;
