93d140384d2e9e2247226c2f2219d718e8874de9
Ewerton Motta <contato@ewerton.dev>
2024-03-03 15:26:34 -0300

    phpstan

--- back-end/app/Models/Post.php
+++ back-end/app/Models/Post.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +12,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function publishedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y \à\s H:i'),
+        );
+    }
 }
