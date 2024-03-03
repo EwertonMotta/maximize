75b106d4537ebc4551de5634dca651212ed5de01
Ewerton Motta <contato@ewerton.dev>
2024-03-03 15:26:21 -0300

    laravel pint

--- back-end/app/Http/Resources/PostResource.php
+++ back-end/app/Http/Resources/PostResource.php
@@ -5,19 +5,31 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Undocumented class.
+ *
+ * @property-read int $id
+ * @property-read string $title
+ * @property-read string $slug
+ * @property-read string $image
+ * @property-read string $description
+ * @property-read string $body
+ * @property-read string $published_at
+ * @property-read string $url
+ */
 class PostResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-return [
+        return [
             'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->slug,
             'image' => $this->image,
             'description' => $this->description,
             'body' => $this->body,
-            'published_at' => 'Publicado em ' . $this->created_at->format('d/m/Y \à\s H:i'),
-            'url' => '/post/' . $this->slug
+            'published_at' => 'Publicado em '.$this->published_at,
+            'url' => '/post/'.$this->slug,
         ];
     }
 }
